559bb085955af2fa879f281e80c5154637055aba
Ismo Vuorinen <ismo@ivuorinen.net>
2023-05-04 15:42:41 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Cinode OpenAPI TypeScript API
+
+-   Types created with [drwpow/openapi-typescript](https://github.com/drwpow/openapi-typescript)
+-   API Examples with [drwpow/openapi-fetch](https://github.com/drwpow/openapi-fetch)
---
+++ generate.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#
+# Generate Cinode TypeScript type definitions
+
+SRC="https://api.cinode.com/swagger/v0.1/swagger.json"
+DEST="$(pwd)/src/cinode.d.ts"
+
+npx openapi-typescript \
+  "$SRC" \
+  --export-type \
+  --path-params-as-type \
+  --output "$DEST"
